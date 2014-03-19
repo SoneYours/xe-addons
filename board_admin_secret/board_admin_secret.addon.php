653fcef15fcdcb81a77d840d3bd60fd0f9b1d796
BJRambo <sosifam@070805.co.kr>
2014-03-19 21:51:07 +0900

    NOISSUES 관리자권한을 게시판관리자가 아닌 최고관리자만 사용할 수 있도록 바꿈

--- board_admin_secret/board_admin_secret.addon.php
+++ board_admin_secret/board_admin_secret.addon.php
@@ -7,6 +7,8 @@
 if(!defined('__XE__'))
 	exit();
 
+$logged_info = Context::get('logged_info');
+
 if($called_position=="before_module_proc" && $this->module=="board")
 {
 	// change board grant about comment
@@ -14,7 +16,7 @@ if($called_position=="before_module_proc" && $this->module=="board")
 		'dispBoardModifyComment',
 		'dispBoardWrite',
 	);
-	if($this->grant->manager && in_array($this->act, $arrCheckAct)) 
+	if($logged_info->is_admin == 'Y' && in_array($this->act, $arrCheckAct)) 
 	{
 		$this->module_info->secret="Y";
 	}
@@ -29,7 +31,7 @@ if($called_position=="after_module_proc" && $this->module=="board")
 	);
 	if(in_array($this->act, $arrCheckAct)) 
 	{
-		if($this->grant->manager)
+		if($logged_info->is_admin == 'Y')
 		{
 			$status_list = Context::get('status_list');
 			if(!isset($status_list['SECRET']))
